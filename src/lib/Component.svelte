e944e208ad45299b6ff82b0ad3edff2123655058
ali akhtarzada <ali.akhtarzada@gmail.com>
2022-02-15 01:07:45 +0100

    Initial commit

---
+++ src/lib/Component.svelte
@@ -0,0 +1,25 @@
+<script context="module" lang="ts">
+	type Events = {
+		declaredType: {
+			value: string;
+		};
+	};
+</script>
+
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	const dispatch1 = createEventDispatcher<Events>();
+	const dispatch2 = createEventDispatcher<{
+		inlineType: {
+			value: string;
+		};
+	}>();
+</script>
+
+<button on:click={() => dispatch1('declaredType', { value: 'hello' })}>
+	Dispatch with declared type
+</button>
+
+<button on:click={() => dispatch2('inlineType', { value: 'hello' })}>
+	Dispatch with inline type
+</button>
---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Component from '$lib/Component.svelte';
+</script>
+
+<Component
+	on:declaredType={(e) => {
+		console.log(e.detail.gagagaga);
+	}}
+	on:inlineType={(e) => {
+		console.log(e.detail.gagagaga);
+	}}
+/>
